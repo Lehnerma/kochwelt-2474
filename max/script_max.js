9e84eb7117fd48581e29a52a2b130751b8ca59a9
Max Lehner <150380803+Lehnerma@users.noreply.github.com>
2025-12-25 21:31:45 +0100

    fisch the js file for getting the table + getting the factor of the quantity with an input.

---
+++ max/script_max.js
@@ -0,0 +1,29 @@
+let quantiy = document.getElementById('inp_quantity');
+let table = document.getElementById('recipe_body');
+let tiramisu =[
+    {amount: 125, unit: "g", ingridient: "Mascarpone"},
+    {amount: 25, unit: "g", ingridient: "Staubzucker"},
+    {amount: 50, unit: "g", ingridient: "Biskotten"},
+    {amount: 1, unit: "stk", ingridient: "Eigelb"},
+    {amount: 0.5, unit: "stk", ingridient: "Eiweiß"},
+    {amount: 1, unit: "Tasse", ingridient: "Kaffee abgekühlt"},
+    {amount: 0.5, unit: "EL", ingridient: "Amaretto oder nach Geschmack auch mehr"},
+    {amount: 1, unit: "EL" ,ingridient: "Kakaopulver ungesüßt, zum bestreuen"}
+];
+function getRecipe(factor){
+    factor = document.getElementById('inp_quantity').value;
+    console.log(factor);
+    table.innerHTML = '';
+    for (item in tiramisu){
+        let row =    
+        `
+        <tr class="recipe_row">
+            <td class="recipe_amount">${tiramisu[item].amount * factor} ${tiramisu[item].unit} </td>
+            <td class="recipe_ingridient">${tiramisu[item].ingridient}</td>
+        </tr>
+        `;
+        table.innerHTML += row;
+        console.log(row);
+        
+    }
+};
