40eb8e3709306283583b76183347c13e30236d31
Max Lehner <150380803+Lehnerma@users.noreply.github.com>
2025-12-28 18:42:03 +0100

    making some changes and try somthing new

--- max/script_max.js
+++ max/script_max.js
@@ -33,3 +33,37 @@ function getRecipe(factor){
         table.innerHTML += row;
     }
 };
+
+function sendMail(event){
+    event.preventDefault();
+    const data = new FormData(event.target);
+    console.log(data);
+    
+    fetch("https://formspree.io/f/mregbrpq", {
+        method: "POST",
+        body: new FormData(event.target),
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(() => {
+        window.location.href = "./recipe_tiramisu.html"; // adding target ! 
+    }).catch((error) => {
+        console.log(error);
+    });
+}
+function sendMail(event){﻿
+    event.preventDefault();
+    const data = new FormData(event.target);
+
+    fetch("YOUR_URL", {
+        method: "POST",
+        body: new FormData(event.target),
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(() => {
+        window.location.href = "./send_mail.html";
+    }).catch((error) => {
+        console.log(error);
+    });
+}
