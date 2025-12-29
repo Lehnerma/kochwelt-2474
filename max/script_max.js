571b97e5e95afb6c682b3eeecd39bca6ed4c4b8b
Max Lehner <150380803+Lehnerma@users.noreply.github.com>
2025-12-29 13:10:29 +0100

    changes in my style file / adding the twitter bird logo to the folder / adding last styling to the contact file / testing the form to send mails to my adress

--- max/script_max.js
+++ max/script_max.js
@@ -1,69 +1,54 @@
-let quantiy = document.getElementById('inp_quantity').value;
-let table = document.getElementById('recipe_body');
-let tiramisu =[
-    {amount: 125, unit: "g", ingridient: "Mascarpone"},
-    {amount: 25, unit: "g", ingridient: "Staubzucker"},
-    {amount: 50, unit: "g", ingridient: "Biskotten"},
-    {amount: 1, unit: "stk", ingridient: "Eigelb"},
-    {amount: 0.5, unit: "stk", ingridient: "Eiweiß"},
-    {amount: 1, unit: "Tasse", ingridient: "Kaffee abgekühlt"},
-    {amount: 0.5, unit: "EL", ingridient: "Amaretto oder nach Geschmack auch mehr"},
-    {amount: 1, unit: "EL" ,ingridient: "Kakaopulver ungesüßt, zum bestreuen"}
+let quantiy = document.getElementById("inp_quantity").value;
+let table = document.getElementById("recipe_body");
+let tiramisu = [
+  { amount: 125, unit: "g", ingridient: "Mascarpone" },
+  { amount: 25, unit: "g", ingridient: "Staubzucker" },
+  { amount: 50, unit: "g", ingridient: "Biskotten" },
+  { amount: 1, unit: "stk", ingridient: "Eigelb" },
+  { amount: 0.5, unit: "stk", ingridient: "Eiweiß" },
+  { amount: 1, unit: "Tasse", ingridient: "Kaffee abgekühlt" },
+  { amount: 0.5, unit: "EL", ingridient: "Amaretto oder nach Geschmack auch mehr" },
+  { amount: 1, unit: "EL", ingridient: "Kakaopulver ungesüßt, zum bestreuen" },
 ];
-function getQuantity(){
-    let value = document.getElementById('inp_quantity').value;
-    if (value == "" || value == 0){
-        value = 1
-        return value
-    } else {
-        return value
-    }
+function getQuantity() {
+  let value = document.getElementById("inp_quantity").value;
+  if (value == "" || value == 0 || value >= 20 || value <= 0) {
+    value = 1;
+    return value;
+  } else {
+    return value;
+  }
 }
-function getRecipe(factor){
-    factor = getQuantity();
-    table.innerHTML = '';
-    for (item in tiramisu){
-        let row =    
-        `
+function getRecipe(factor) {
+  factor = getQuantity();
+  table.innerHTML = "";
+  for (item in tiramisu) {
+    let row = `
         <tr class="recipe_row">
             <td class="recipe_amount">${tiramisu[item].amount * factor} ${tiramisu[item].unit} </td>
             <td class="recipe_ingridient">${tiramisu[item].ingridient}</td>
         </tr>
         `;
-        table.innerHTML += row;
-    }
-};
-
-function sendMail(event){
-    event.preventDefault();
-    const data = new FormData(event.target);
-    console.log(data);
-    
-    fetch("https://formspree.io/f/mregbrpq", {
-        method: "POST",
-        body: new FormData(event.target),
-        headers: {
-            'Accept': 'application/json'
-        }
-    }).then(() => {
-        window.location.href = "./recipe_tiramisu.html"; // adding target ! 
-    }).catch((error) => {
-        console.log(error);
-    });
+    table.innerHTML += row;
+  }
 }
-function sendMail(event){﻿
-    event.preventDefault();
-    const data = new FormData(event.target);
 
-    fetch("YOUR_URL", {
-        method: "POST",
-        body: new FormData(event.target),
-        headers: {
-            'Accept': 'application/json'
-        }
-    }).then(() => {
-        window.location.href = "./send_mail.html";
-    }).catch((error) => {
-        console.log(error);
+function sendMail(event) {
+  event.preventDefault();
+  const data = new FormData(event.target);
+  console.log(data);
+
+  fetch("https://formspree.io/f/mregbrpq", {
+    method: "POST",
+    body: new FormData(event.target),
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then(() => {
+      window.location.href = "./index.html"; // landing page after submit
+    })
+    .catch((error) => {
+      console.log(error);
     });
 }
